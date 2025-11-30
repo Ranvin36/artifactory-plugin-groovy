2b643d5630cbdb4549c24a1a970247086b649323
ranvin-svg <ranvin@wso2.com>
2025-11-30 20:42:40 +0530

    Validation Logic Updated

--- src/Main.groovy
+++ src/Main.groovy
@@ -8,21 +8,26 @@ storage{
         def pathString = item.getRepoPath().toString();
         def repo = item.getRepoKey();
         def fileName = path.getName();
-//        log.warn(JsonOutput.prettyPrint(JsonOutput.toJson(item.properties)))
 
         def folderPath = pathString.substring(0, pathString.lastIndexOf('/') + 1);
-
         def ballerinaFilePath = RepoPathFactory.create(repo, folderPath + fileName);
+        log.warn("ballerinaFilePath: $ballerinaFilePath");
+
+        if (item.folder) {
+            log.warn("Skipping folder creation: ${item.repoPath}")
+            return
+        }
+
         if(repositories.exists(ballerinaFilePath)){
             throw new Exception("Folder $ballerinaFilePath already exists.$item");
         }
 
-//        if(!path.toString().endsWith('.bala')){
-//            throw new Exception("Uploading files other than .bala is not allowed.$item");
-//        }
-//        else{
-//            log.info("Uploading .bala file to repo: $repo at path: $path");
-//            return;
-//        }
+        if(!path.toString().endsWith('.bala')){
+            throw new Exception("Uploading files other than .bala is not allowed.$item");
+        }
+        else{
+            log.info("Uploading .bala file to repo: $repo at path: $path");
+            return;
+        }
     }
 }
